8a0e6346f81ea0edaa775906abbb6261775bd1b6
Salomeja <salomeja78@gmail.com>
2018-12-11 17:26:32 +0200

    login

---
+++ js/scripts.js
@@ -0,0 +1,12 @@
+//slaptazodis
+// Get the login modal
+var modal = document.getElementById('id01');
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+
+// kita
